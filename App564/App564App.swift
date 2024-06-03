8645f8f5f0e9db8f2d336b2af1f2c834c2385a11
DJUROM&&& <131347905+IGOLIVIT@users.noreply.github.com>
2024-06-03 11:32:58 +0300

    Initial Commit

---
+++ App564/App564App.swift
@@ -0,0 +1,17 @@
+//
+//  App564App.swift
+//  App564
+//
+//  Created by IGOR on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct App564App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
